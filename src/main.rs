c7ee79539eaab27d492cae206dd544dbe78d33fc
Samuel Ortiz <s.ortiz@apple.com>
2022-01-09 09:29:09 +0100

    run0: chroot to the container rootfs
    Signed-off-by: Samuel Ortiz <s.ortiz@apple.com>

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 use clap::Parser;
 use unshare::Namespace;
 
+use std::path::PathBuf;
+
+const OCI_RUNTIME_SPEC_FILE: &str = "config.json";
+const OCI_RUNTIME_SPEC_ROOTFS: &str = "rootfs";
+
 #[derive(Parser)]
 #[clap(version = "0.1", author = "Polytech Montpellier - DevOps")]
 struct RuntimeOpts {
@@ -18,13 +23,32 @@ pub enum Error {
     ChildExitError(i32),
 }
 
+pub type Result<T> = std::result::Result<T, Error>;
+
+struct Runtime {
+    rootfs: PathBuf,
+    spec_file: PathBuf,
+}
+
+impl Runtime {
+    pub fn new(bundle: &str) -> Result<Self> {
+        Ok(Runtime {
+            rootfs: [bundle, OCI_RUNTIME_SPEC_ROOTFS].iter().collect(),
+            spec_file: [bundle, OCI_RUNTIME_SPEC_FILE].iter().collect(),
+        })
+    }
+}
+
 fn main() -> Result<()> {
     let opts: RuntimeOpts = RuntimeOpts::parse();
     let mut namespaces = Vec::<Namespace>::new();
 
     namespaces.push(Namespace::Pid);
 
+    let runtime = Runtime::new(&opts.bundle)?;
+
     let code = unshare::Command::new("/bin/sh")
+        .chroot_dir(&runtime.rootfs)
         .unshare(&namespaces)
         .spawn()
         .map_err(Error::CmdSpawn)?

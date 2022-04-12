20cb2eb5b687c30a5c571f9e62a4fb8ce268fe4b
kalil <kalil.pelissier@gmail.com>
2022-04-13 00:41:56 +0200

    kaps: Add id parameter to run command
    Introduce a required id parameter to the cli run command.
    Add usage of the container state implementation.
    
    Signed-off-by: kalil <kalil.pelissier@gmail.com>

--- src/cli/run.rs
+++ src/cli/run.rs
@@ -14,6 +14,10 @@ use container::Container;
 /// The `handler` method provided below will be executed.
 #[derive(Debug, Args)]
 pub struct RunCommand {
+    /// Name of the container instance that will be start. It must me unique on your host
+    #[clap(name = "container-id")]
+    id: String,
+
     /// The bundle used by the container.
     #[clap(short, long)]
     bundle: String,
@@ -22,7 +26,7 @@ pub struct RunCommand {
 impl Handler for RunCommand {
     fn handler(&self) -> Result<()> {
         // Create a container by passing the bundle provided in arguments to it's constructor.
-        let container = Container::new(&self.bundle)?;
+        let mut container = Container::new(&self.bundle, &self.id)?;
 
         // Run the container
         // At the moment, we don't have a detached mode for the container,
